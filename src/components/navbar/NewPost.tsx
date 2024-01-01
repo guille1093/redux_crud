e1ce7b0ec0f519c6df2fada538a533ea93306149
guille1093 <guille121093@gmail.com>
2024-01-01 19:57:19 -0300

    Crear Post con imagen funciona :D

--- src/components/navbar/NewPost.tsx
+++ src/components/navbar/NewPost.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNewPost } from '../../store/posts/postsSlice';
+import type { Post } from '../../store/posts/postsSlice';
 
 function NewPost() {
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const [selectedImage, setSelectedImage] = useState<string>('');
+    const dispatch = useDispatch();
+    const [caption, setCaption] = useState<string>('');
+    const [photo, setPhoto] = useState<string>('');
+    const [img, setImg] = useState<File | null>(null);
+
+    const handleCaptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setCaption(event.target.value);
+    }
 
     const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -11,8 +22,27 @@ function NewPost() {
                 setSelectedImage(reader.result as string);
             };
             reader.readAsDataURL(file);
+            setPhoto(file.name);
+            setImg(file);
+        } else {
+            setSelectedImage('');
+            setPhoto('');
         }
     };
+
+    const savePost = () => {
+        if (caption && photo) {
+            const post: Post = {
+                caption: caption,
+                img: img
+            }
+            dispatch(addNewPost({ post }));
+            setCaption('');
+            setPhoto('');
+            setSelectedImage('');
+            (document.getElementById('newPost') as HTMLDialogElement).close();
+        }
+    }
 
     return (
         <>
@@ -24,20 +54,19 @@ function NewPost() {
             <dialog id="newPost" className="modal">
                 <div className="modal-box border-base-content border-2">
                     <h3 className="font-bold text-lg">Nuevo post</h3>
-                    {/* form para un nuevo post con un input para subir una imagen y un textfield para la caption */}
                     <form className="form-control">
                         <label className="label">
                             <span className="label-text">Imagen</span>
                         </label>
-                        <input type="file" required className="file-input w-full" onChange={handleImageChange} />
+                        <input type="file" id="photo" name="photo" required className="file-input w-full" onChange={handleImageChange} />
                         {selectedImage && (
                             <img src={selectedImage} alt="Vista previa de la imagen" className="w-full mt-2 rounded-xl" />
                         )}
                         <label className="label">
                             <span className="label-text">Descripción</span>
                         </label>
-                        <textarea required className="textarea textarea-bordered"></textarea>
-                        <button className="btn mt-4 btn-primary w-full">Publicar</button>
+                        <textarea id="caption" name="caption" required className="textarea textarea-bordered" value={caption} onChange={handleCaptionChange}></textarea>
+                        <button className="btn mt-4 btn-primary w-full" onClick={savePost}>Publicar</button>
                     </form>
                     <button className="btn mt-4 btn-outline w-full" onClick={() => (document.getElementById('newPost') as HTMLDialogElement).close()}>Cerrar</button>
                 </div>
--- src/store/posts/postsSlice.ts
+++ src/store/posts/postsSlice.ts
@@ -2,6 +2,11 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const POST_URL = "https://gq-pfs.pockethost.io/api/collections/posts/records";
 
+export type Post = {
+    img: File | null;
+    caption: string;
+};
+
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
     // Simulate network latency
     await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -9,11 +14,16 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
     return response.data;
 });
 
-export const addNewPost = createAsyncThunk("posts/addNewPost", async (initialPost) => {
-    const response = await axios.post(POST_URL, initialPost);
+export const addNewPost = createAsyncThunk("posts/addNewPost", async (initialPost: { post: Post }) => {
+    const newPost: Post = initialPost.post;
+    //newPost.img = null;
+    const response = await axios.post(POST_URL, newPost, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
     return response.data;
-}
-);
+});
 
 export const postsSlice = createSlice({
     name: "posts",
@@ -39,8 +49,8 @@ export const postsSlice = createSlice({
             .addCase(addNewPost.pending, (state) => {
                 state.status = "loading";
             })
-            .addCase(addNewPost.fulfilled, (state, action) => {
-                state.posts = action.payload;
+            .addCase(addNewPost.fulfilled, (state) => {
+                state.status = "succeeded";
             })
             .addCase(addNewPost.rejected, (state, action) => {
                 state.error = action.error.message;
