9a0344348d629c9e4a326f250264d72590f859a1
guille1093 <guille121093@gmail.com>
2024-01-02 15:25:14 -0300

    imports

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,4 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchPosts } from "../store/posts/postsSlice";
 
